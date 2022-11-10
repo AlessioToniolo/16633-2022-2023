b6e46c44a601eca9c51684734b2f7a3095cfbfee
Books4life01 <96316044+Books4life01@users.noreply.github.com>
2022-11-10 18:32:30 +0000

    checking out github workspaces; pretty cool; added class to easily wind up the strings on the sliders

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/tele/SliderReStringerTeleop.java
@@ -0,0 +1,22 @@
+package org.firstinspires.ftc.teamcode.tele;
+
+import org.firstinspires.ftc.teamcode.utility.BaseRobot;
+
+
+
+@Teleop
+public class SliderReStringerTeleop extends LinearOpMode{
+
+
+    BaseRobot robot = new BaseRobot();
+    double sliderPower=0;
+    double sideSliderPower=0;
+    @Override
+    public void runOpMode(){
+
+        sliderPower=gamepad2.left_stick_y/2.0;
+        sideSliderPower=gamepad2.right_stick_y/2.0;
+        robot.sideSlider.setPower(sideSliderPower);
+        robot.slider.setPower(sliderPower);
+    }
+}

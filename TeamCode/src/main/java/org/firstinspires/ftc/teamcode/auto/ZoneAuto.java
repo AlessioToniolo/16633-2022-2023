d0d5cf82176018c84732eabe9b769e743c435885
ColeSaunders25 <colesaunders25@marist.com>
2022-11-16 12:19:36 -0500

    changed Zone Auto to use roadrunner

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auto/ZoneAuto.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auto/ZoneAuto.java
@@ -1,9 +1,13 @@
 package org.firstinspires.ftc.teamcode.auto;
+import com.acmerobotics.roadrunner.geometry.Pose2d;
+import com.acmerobotics.roadrunner.geometry.Vector2d;
+import com.acmerobotics.roadrunner.trajectory.Trajectory;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.util.ElapsedTime;
 
 import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
+import org.firstinspires.ftc.teamcode.drive.SampleMecanumDrive;
 import org.firstinspires.ftc.teamcode.utility.BaseRobot;
 import org.firstinspires.ftc.teamcode.utility.Fields;
 import org.firstinspires.ftc.teamcode.utility.pipelines.ZoneDetectionPipeline;
@@ -23,6 +27,8 @@ public class ZoneAuto extends LinearOpMode{
     @Override
     public void runOpMode() throws InterruptedException {
         robot.init(hardwareMap);
+        SampleMecanumDrive drive = new SampleMecanumDrive(hardwareMap);
+
         webcamName = hardwareMap.get(WebcamName .class, "Webcam 1");
         camera = OpenCvCameraFactory.getInstance().createWebcam(webcamName);
         myPipeline = new ZoneDetectionPipeline(telemetry, Fields.subRectX,Fields.subRectY,Fields.subRectWidth,Fields.subRectHeight);
@@ -44,95 +50,34 @@ public class ZoneAuto extends LinearOpMode{
         camera.stopStreaming();
         camera.closeCameraDevice();
 
-        straight();
-        if(zone==1)left();
-        else if(zone ==3)right();
+        // Build Trajectories
+        Pose2d startPose = new Pose2d(36, -60, Math.toRadians(90));
+        drive.setPoseEstimate(startPose);
 
 
 
-        /**
-         robot.strafeInches(1,36,5);
-         delay(.5);
-         robot.pointTurnDegrees(1,180,5);
-         delay(.5);
-         robot.slider.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-         robot.slider.setTargetPosition(Fields.sliderHighJunctionLevel);
-         robot.arm.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-         robot.arm.setTargetPosition(Fields.armDepositForward);
-         robot.arm.setPower(1);
-         robot.slider.setPower(1);
-         delay(2);
-         robot.leftClaw.setPosition(Fields.leftClawDeliver);
-         robot.rightClaw.setPosition(Fields.rightClawDeliver);
-         delay(1);
-         robot.leftClaw.setPosition(Fields.rightClawPickup);
-         robot.rightClaw.setPosition(Fields.leftClawPickup);
-         //set claw to open a;jl;oiewqoia;lkjdsa;lkjfdsa
-         //set claw to close ;LASJFDKA;LJSDFKJJDSAF
-         robot.arm.setTargetPosition(0);
-         robot.slider.setTargetPosition(0);
-         robot.arm.setPower(1);
-         robot.slider.setPower(1);
-         if(zone==1)
-         {
-         robot.strafeInches(1,-24,5);
-         }
-         else if(zone==3)
-         {
-         robot.strafeInches(1,24,5);
-         }**/
+        // Zone trajs
+        Trajectory zone2 = drive.trajectoryBuilder(startPose)
+                .lineTo(new Vector2d(36, -12))
+                .build();
+        Trajectory zone3 = drive.trajectoryBuilder(zone2.end())
+                .lineToLinearHeading(new Pose2d(69, -12, Math.toRadians(90)))
+                .build();
+
+        Trajectory zone1 = drive.trajectoryBuilder(zone2.end())
+                .lineToLinearHeading(new Pose2d(2.3, -12, Math.toRadians(90)))
+                .build();
+
+        drive.followTrajectory(zone2);
+        if(zone==3)drive.followTrajectory(zone3);
+        else if(zone ==1)drive.followTrajectory(zone1);
+
+
+
     }
     public void delay(double t) { // Imitates the Arduino delay function
         runtime.reset();
         while (opModeIsActive() && (runtime.seconds() < t)) {
         }
-    }
-    public void straight(){
-        robot.drive.leftFront.setPower(1);
-        robot.drive.rightFront.setPower(1);
-        robot.drive.leftRear.setPower(1);
-        robot.drive.rightRear.setPower(1);
-        delay(.6);
-
-        robot.drive.leftFront.setPower(0);
-        robot.drive.rightFront.setPower(0);
-        robot.drive.leftRear.setPower(0);
-        robot.drive.rightRear.setPower(0);
-    }
-    public void backward(){
-        robot.drive.leftFront.setPower(-1);
-        robot.drive.rightFront.setPower(-1);
-        robot.drive.leftRear.setPower(-1);
-        robot.drive.rightRear.setPower(-1);
-        delay(.5);
-
-        robot.drive.leftFront.setPower(0);
-        robot.drive.rightFront.setPower(0);
-        robot.drive.leftRear.setPower(0);
-        robot.drive.rightRear.setPower(0);
-    }
-    public void right(){
-        robot.drive.leftFront.setPower(1);
-        robot.drive.rightFront.setPower(-1);
-        robot.drive.leftRear.setPower(-1);
-        robot.drive.rightRear.setPower(1);
-        delay(.8);
-
-        robot.drive.leftFront.setPower(0);
-        robot.drive.rightFront.setPower(0);
-        robot.drive.leftRear.setPower(0);
-        robot.drive.rightRear.setPower(0);
-    }
-    public void left(){
-        robot.drive.leftFront.setPower(-1);
-        robot.drive.rightFront.setPower(1);
-        robot.drive.leftRear.setPower(1);
-        robot.drive.rightRear.setPower(-1);
-        delay(.8);
-
-        robot.drive.leftFront.setPower(0);
-        robot.drive.rightFront.setPower(0);
-        robot.drive.leftRear.setPower(0);
-        robot.drive.rightRear.setPower(0);
     }
 }
